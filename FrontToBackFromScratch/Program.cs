caa5a7c63dc215091314834ca5ea9deafcf3976a
malinborn <rikkeisy@gmail.com>
2023-11-01 22:31:02 +0300

    initial commit

---
+++ FrontToBackFromScratch/Program.cs
@@ -0,0 +1,17 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System.Runtime.InteropServices.ComTypes;
+
+var builder = WebApplication.CreateBuilder();
+
+var app = builder.Build();
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+app.UseRouting();
+
+app.MapFallbackToFile("index.html");
+
+app.MapGet("/hello", (CancellationToken ct) => "Do you remember me?");
+
+app.Run();
